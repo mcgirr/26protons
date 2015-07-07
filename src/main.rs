6eec268a4932402781d4cbf8cc634c74730b1f1b
michaelmcgirr <michaelgordonmcgirr@gmail.com>
2015-07-06 19:03:37 -0700

    Now serving files from the html directory rather than from somewhere in the target directory.

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,6 @@ extern crate iron;
 extern crate staticfile;
 extern crate mount;
 
-// This example serves the docs from target/doc/staticfile at /doc/
-
-// TODO figure out how this works
-// Run `cargo doc && cargo test && ./target/doc_server`, then
-// point your browser to http://127.0.0.1:3000/doc/
-
 use std::path::Path;
 
 use iron::Iron;
@@ -17,16 +11,16 @@ use mount::Mount;
 fn main() {
     let mut mount = Mount::new();
 
-    // Serve the shared JS/CSS at /
-    mount.mount("/", Static::new(Path::new("target/doc/")));
+    // Serve the files from the html dir at /
+    mount.mount("/", Static::new(Path::new("html/")));
     
     // Serve the static file docs at /doc/
-    mount.mount("/doc/", Static::new(Path::new("target/doc/staticfile/")));
+    //mount.mount("/doc/", Static::new(Path::new("target/doc/staticfile/")));
     
     // Serve the source code at /src/
-    mount.mount("/src/", Static::new(Path::new("target/doc/src/staticfile/lib.rs.html")));
+    //mount.mount("/src/", Static::new(Path::new("target/doc/src/staticfile/lib.rs.html")));
 
-    println!("Doc server running on http://localhost:3000/doc/");
+    println!("Doc server running on http://localhost:3000/");
 
     Iron::new(mount).http("127.0.0.1:3000").unwrap();
 }
